f5a274cdcdbb8bcb893e4e43b62ec05e2b7b3291
Luis Antonio Molina Yampa <antoniomolina.yampa@gmail.com>
2023-08-29 08:03:23 -0400

    Declaracion de constructores

--- Dados.cpp
+++ Dados.cpp
@@ -4,12 +4,28 @@ class Dice
 {
 	//* modificadores de acceso
 private:
-	
+	int target;
 public:
+	int value;
 	//declarion de atributos
 	
 	//declar constructores
-	
+	//El constructor siempre lleva el mismo nombre de la clase
+	//comunmente sirve para inicializar los atributos
+	Dice()
+	{
+		target = 5;
+		value = 0;
+	}
+	Dice(int tar)
+	{
+		target = tar;
+	}
+	Dice(int val, int tar)
+	{
+		target = tar;
+		value = val;
+	}
 	//declara metodos
 	
 };
@@ -25,9 +41,14 @@ struct Hora
 };
 
 int main(int argc, char *argv[]) {
-	Hora h1;
-	h1.minuto =7;
+	//Hora h1;
+	//h1.minuto =7;
 	//cout<<h1.hora<<":"<<h1.minuto<<":"<<h1.segundo<<endl;
+	Dice d1;
+	cout<<d1.value<<endl;
+	
+	Dice d2;
+	cout<<d2.value;
 	return 0;
 }
 
