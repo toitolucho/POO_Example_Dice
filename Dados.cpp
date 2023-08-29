1415e70f4b8a08e64e469913598429d9fcc6294b
Luis Antonio Molina Yampa <antoniomolina.yampa@gmail.com>
2023-08-29 07:35:01 -0400

    Plantilla Base

---
+++ Dados.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+class Dice
+{
+	//* modificadores de acceso
+private:
+	
+public:
+	//declarion de atributos
+	
+	//declar constructores
+	
+	//declara metodos
+	
+};
+
+struct Hora
+{
+//public:
+	int hora;
+	int minuto;
+	int segundo;
+//private:
+	char tipo;
+};
+
+int main(int argc, char *argv[]) {
+	Hora h1;
+	h1.minuto =7;
+	//cout<<h1.hora<<":"<<h1.minuto<<":"<<h1.segundo<<endl;
+	return 0;
+}
+
